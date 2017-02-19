6bb2a6c0f38c2d073ae7b35f6800e439f7d02619
bluuuuer <xuwei1993@qq.com>
2017-02-19 18:07:01 +0800

    Update 438.cpp

--- 438.cpp
+++ 438.cpp
@@ -34,21 +34,19 @@ class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
         vector<int> pv(256,0), sv(256,0), res;
-        if(s.size() < p.size())
+        if (s.size() < p.size())
            return res;
-        for(int i = 0; i < p.size(); ++i)
-        {
-            ++pv[p[i]];
-            ++sv[s[i]];
+        for (int i = 0; i < p.size(); ++i) {
+            pv[p[i]] ++;
+            sv[s[i]] ++;
         }
         if(pv == sv)
            res.push_back(0);
-        for(int i = p.size(); i < s.size(); ++i)
-        {
-            ++sv[s[i]];
-            --sv[s[i-p.size()]];
+        for (int i = p.size(); i < s.size(); i ++) {
+            sv[s[i]] ++;
+            sv[s[i - p.size()]] --;
             if(pv == sv)
-               res.push_back(i-p.size()+1);
+               res.push_back(i - p.size() + 1);
         }
         return res;
     }
