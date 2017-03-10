9d0bf745c366dcb080cadb4c85f9faa15d19d051
bluuuuer <xuwei1993@qq.com>
2017-03-10 16:42:56 +0800

    Update 495.cpp

--- 495.cpp
+++ 495.cpp
@@ -23,16 +23,16 @@ So you finally need to output 3.
 class Solution {
 public:
     int findPoisonedDuration(vector<int>& timeSeries, int duration) {
+        if (timeSeries.size() == 0)
+            return 0;
         int res = 0;
-        for (int i = 0; i < timeSeries.size(); i ++) {
-            if (i < timeSeries.size() - 1) {
-                if (timeSeries[i + 1] - timeSeries[i] < duration)
-                    res += timeSeries[i + 1] - timeSeries[i];   
-                else
-                    res += duration;
-            } else 
+        for (int i = 0; i + 1 < timeSeries.size(); i ++) {
+            if (timeSeries[i + 1] - timeSeries[i] < duration)
+                res += timeSeries[i + 1] - timeSeries[i];   
+            else
                 res += duration;
         }
+        res += duration;
         return res;
     }
 };
